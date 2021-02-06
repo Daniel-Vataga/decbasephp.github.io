6a3d202b5c1e7aead6701a158481908848890506
Dan <50670353+Daniel-Vataga@users.noreply.github.com>
2021-02-06 21:05:16 +0200

    try to fix dropdown menu 2

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,15 @@ $(document).ready(function () {
 
 		if ($('#burgerBtn').hasClass('open')) {
 			$('.headerNavMobile').css('top', '0');
-
+			$('.headerNavMobile').css('opacity', '1');
 
 			$('body').css('overflow', 'hidden');
 			
 		}
 		else {
 			$('.headerNavMobile').css('top', '-130%');
+			$('.headerNavMobile').css('opacity', '0');
+
 			$('body').css('overflow', 'unset');
 		}
 	});
