b627ca6991cd890e339b251f7848981b89855068
Dan <50670353+Daniel-Vataga@users.noreply.github.com>
2021-02-12 16:19:01 +0200

    add testimonials slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,64 @@
 const burger = document.getElementById('burgerBtn');
 const headerNavMobile = document.querySelector('.headerNavMobile');
 
-function openMenu() {
-	headerNavMobile.style.top = '0';
-	headerNavMobile.style.opacity = '1';
-	document.body.style.overflow = 'hidden';
-}
-
-function closeMenu() {
-	headerNavMobile.style.top = '-100%';
-	headerNavMobile.style.opacity = '0';
-	document.body.style.overflow = 'unset';
-}
-
 burger.addEventListener('click', () => {
 	burger.classList.toggle('open');
 
 	if (burger.classList.contains('open')) {
-		openMenu()
+		headerNavMobile.style.top = '0';
+		headerNavMobile.style.opacity = '1';
+		document.body.style.overflow = 'hidden';
 	} else {
-		closeMenu()
+		headerNavMobile.style.top = '-100%';
+		headerNavMobile.style.opacity = '0';
+		document.body.style.overflow = 'unset';
+	}
+})
+
+const leftArrow = document.querySelector('.leftArrowBtn');
+const rightArrow = document.querySelector('.rightArrowBtn');
+const firstSlide = document.querySelectorAll('.firstSlide');
+const secondSlide = document.querySelectorAll('.secondSlide');
+
+rightArrow.addEventListener('click', () => {
+	rightArrow.setAttribute('disabled', 'disabled');
+	leftArrow.removeAttribute('disabled');
+
+	for (let i = 0; i < firstSlide.length; i++) {
+		firstSlide[i].style.opacity = '0'
+
+		setTimeout(() => {
+			firstSlide[i].style.display = 'none'
+			for (let s = 0; s < secondSlide.length; s++) {
+				secondSlide[s].style.display = 'flex'
+				setTimeout(() => {
+					secondSlide[s].style.opacity = '1'
+				}, 10)
+			}
+		}, 300)
+
+
+	}
+})
+
+leftArrow.addEventListener('click', () => {
+	leftArrow.setAttribute('disabled', 'disabled');
+	rightArrow.removeAttribute('disabled');
+	for (let s = 0; s < secondSlide.length; s++) {
+		secondSlide[s].style.opacity = '0'
+
+		setTimeout(() => {
+			secondSlide[s].style.display = 'none';
+			for (let i = 0; i < firstSlide.length; i++) {
+				firstSlide[i].style.display = 'flex'
+
+				setTimeout(() => {
+					firstSlide[i].style.opacity = '1'
+				}, 10)
+			}
+		}, 300)
+
+
+
 	}
 })
